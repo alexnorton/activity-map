0651819237bf776f4038f79ef6ef7aa99a20a588
Alex Norton <alexnorton@fastmail.com>
2018-01-17 18:02:09 +0100

    Initial implementation of activities routes

--- server/src/routes/activities.js
+++ server/src/routes/activities.js
@@ -1,16 +1,65 @@
 const ensureAuthenticated = require('../helpers/ensureAuthenticated');
 const strava = require('strava-v3');
 
+const { Activity, User } = require('../model');
+
 module.exports = (app) => {
   app.get('/activities', ensureAuthenticated, (req, res) => {
-    strava.athlete.listActivities({
-      access_token: req.user.json.token,
-    }, (error, result, limits) => {
-      if (error) {
-        res.status(500).json({ error });
-        return;
+    strava.athlete.listActivities(
+      {
+        access_token: req.user.json.token
+      },
+      (error, result, limits) => {
+        if (error) {
+          res.status(500).json({ error });
+          return;
+        }
+        res.json(result);
       }
-      res.json(result);
-    });
+    );
+  });
+
+  app.get('/activities/strava', ensureAuthenticated, (req, res) => {
+    strava.athlete.listActivities(
+      {
+        access_token: req.user.json.token
+      },
+      (error, result) => {
+        if (error) {
+          res.status(500).json({ error });
+          return;
+        }
+        res.json(result);
+      }
+    );
+  });
+
+  app.get('/activities/:activityId', ensureAuthenticated, (req, res) => {
+    const activityId = parseInt(req.params.activityId, 10);
+    Activity.findById(activityId)
+      .then((activity) => {
+        if (!activity) {
+          return new Promise((resolve, reject) => {
+            strava.activities.get(
+              {
+                id: activityId,
+                access_token: req.user.json.token
+              },
+              (error, result) => {
+                if (error) {
+                  reject(error);
+                }
+                resolve(result);
+              }
+            );
+          }).then(stravaActvity =>
+            Activity.create({ id: stravaActvity.id, json: stravaActvity, userId: req.user.id })
+          );
+        }
+        return activity;
+      })
+      .then((activity) => {
+        res.json(activity);
+      });
   });
 };
