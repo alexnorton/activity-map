89a2f3700af5e44705f72336f271c3dc4ad425ec
Alex Norton <alexnorton@fastmail.com>
2018-01-26 11:30:49 +0000

    Add Redux devtools extension support

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,6 +1,11 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 
-const configureStore = () => createStore(rootReducer);
+const configureStore = () => (
+  createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  )
+);
 
 export default configureStore;
