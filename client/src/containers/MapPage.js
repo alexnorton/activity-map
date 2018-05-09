5a30c7c270cc999262adfd33a9dffec2bef0c8bb
Alex Norton <alexnorton@fastmail.com>
2018-05-09 22:27:35 +0100

    Display activities on map

--- client/src/containers/MapPage.js
+++ client/src/containers/MapPage.js
@@ -1,24 +1,64 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ReactMapGL from 'react-map-gl';
-import DeckGL, { LineLayer, ScatterplotLayer } from 'deck.gl';
+import DeckGL, { GeoJsonLayer } from 'deck.gl';
 import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer';
+import polyline from '@mapbox/polyline';
+import PropTypes from 'prop-types';
+
+import { fetchActivitiesFromCache } from '../actions';
 
 class MapPage extends React.Component {
+  static propTypes = {
+    activities: PropTypes.shape().isRequired,
+    fetchActivitiesFromCache: PropTypes.func.isRequired,
+  };
+
+  static getDerivedStateFromProps(nextProps) {
+    const { activities } = nextProps;
+
+    if (activities.fetched) {
+      const features = Object.keys(activities.data)
+        .filter(key => activities.data[key].polyline)
+        .map((key) => {
+          const activity = activities.data[key];
+
+          return {
+            type: 'Feature',
+            geometry: polyline.toGeoJSON(activity.polyline),
+            properties: {
+              type: activity.type,
+            },
+          };
+        });
+
+      const geoJson = {
+        type: 'FeatureCollection',
+        features,
+      };
+
+      return { geoJson };
+    }
+
+    return null;
+  }
+
   state = {
     viewport: {
-      latitude: 37.785164,
-      longitude: -122.41669,
-      zoom: 16.14044,
-      bearing: -20.55991,
-      pitch: 60,
+      latitude: 51.507222,
+      longitude: -0.1275,
+      zoom: 10,
     },
   };
+
+  componentDidMount() {
+    this.props.fetchActivitiesFromCache();
+  }
 
   handleViewportChange = viewport => this.setState({ viewport });
 
   render() {
-    const { viewport } = this.state;
+    const { viewport, geoJson } = this.state;
 
     return (
       <div className="flex-grow-1">
@@ -31,17 +71,22 @@ class MapPage extends React.Component {
               height={height}
               width={width}
             >
-              <DeckGL {...viewport} width={width} height={height} debug>
-                <LineLayer
-                  data={[
-                    { sourcePosition: [-122.41669, 37.7883], targetPosition: [-122.41669, 37.781] },
-                  ]}
-                  strokeWidth={5}
-                />
-                <ScatterplotLayer
-                  data={[{ position: [-122.41669, 37.79] }]}
-                  radiusScale={100}
-                  getColor={() => [0, 0, 255]}
+              <DeckGL {...viewport} width={width} height={height}>
+                <GeoJsonLayer
+                  data={geoJson}
+                  pickable
+                  getLineColor={({ properties: { type } }) => {
+                    switch (type) {
+                      case 'Ride':
+                        return [220, 53, 69, 255];
+                      case 'Run':
+                        return [0, 123, 255, 255];
+                      default:
+                        return [40, 167, 69, 255];
+                    }
+                  }}
+                  getLineWidth={() => 2}
+                  lineWidthMinPixels={0.7}
                 />
               </DeckGL>
             </ReactMapGL>
@@ -56,4 +101,8 @@ const mapStateToProps = state => ({
   activities: state.activities,
 });
 
-export default connect(mapStateToProps)(MapPage);
+const mapDispatchToProps = dispatch => ({
+  fetchActivitiesFromCache: () => dispatch(fetchActivitiesFromCache()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MapPage);
