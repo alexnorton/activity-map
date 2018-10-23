cee7b37c3a7569375b54994b851ebe111550a1a0
Alex Norton <alex@alexnorton.com>
2018-10-23 22:18:07 +0100

    Always fetch activities from Strava

--- server/src/routes/activities.js
+++ server/src/routes/activities.js
@@ -9,11 +9,11 @@ module.exports = (app) => {
   app.get('/activities', ensureAuthenticated, async (req, res) => {
     const activities = await req.user.getActivities({
       attributes: [
-        [Sequelize.literal('json->>\'id\''), 'id'],
-        [Sequelize.literal('json->>\'type\''), 'type'],
-        [Sequelize.literal('json->>\'name\''), 'name'],
-        [Sequelize.literal('json->>\'start_date\''), 'start_date'],
-        [Sequelize.literal('json->\'map\'->>\'polyline\''), 'polyline'],
+        [Sequelize.literal("json->>'id'"), 'id'],
+        [Sequelize.literal("json->>'type'"), 'type'],
+        [Sequelize.literal("json->>'name'"), 'name'],
+        [Sequelize.literal("json->>'start_date'"), 'start_date'],
+        [Sequelize.literal("json->'map'->>'polyline'"), 'polyline']
       ]
     });
 
@@ -23,6 +23,7 @@ module.exports = (app) => {
   app.get('/activities/strava', ensureAuthenticated, async (req, res) => {
     const activities = await strava(req.user, 'athlete', 'listActivities', {
       page: parseInt(req.query.page, 10) || 1,
+      per_page: 100
     });
 
     const activityIds = activities.map(({ id }) => id);
@@ -33,15 +34,16 @@ module.exports = (app) => {
   app.get('/activities/:activityId', ensureAuthenticated, async (req, res) => {
     const activityId = parseInt(req.params.activityId, 10);
 
-    let activity = await Activity.findById(activityId);
+    const stravaActvity = await strava(req.user, 'activities', 'get', { id: activityId });
 
-    if (!activity) {
-      const stravaActvity = await strava(req.user, 'activities', 'get', { id: activityId });
-
-      activity = await Activity.create({
-        id: stravaActvity.id, json: stravaActvity, userId: req.user.id
-      });
-    }
+    const [activity] = await Activity.upsert(
+      {
+        id: stravaActvity.id,
+        json: stravaActvity,
+        userId: req.user.id
+      },
+      { returning: true }
+    );
 
     res.json(activity);
   });
